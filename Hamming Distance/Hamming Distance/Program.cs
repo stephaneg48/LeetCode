e9b2c22f5c21f9d3d08b7656aa12074102dff451
Stephane Gilles <sag48@njit.edu>
2023-02-23 16:05:07 -0500

    solved Hamming Distance (C#) and updated gitignore for visual studio dependencies; simple bit-checking problem

---
+++ Hamming Distance/Hamming Distance/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Hamming_Distance
+{
+    public class Solution
+    {
+        public static int HammingDistance(int x, int y)
+        {
+            int res = 0;
+
+            for (int i = 0; i < 32; i++)
+            {
+                if ((x & (1U << i)) != (y & (1U << i)))
+                    res += 1;
+            }
+
+            return res;
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3, y = 1;
+            int result = Solution.HammingDistance(x, y);
+            Console.WriteLine(result);
+        }
+    }
+    
+}

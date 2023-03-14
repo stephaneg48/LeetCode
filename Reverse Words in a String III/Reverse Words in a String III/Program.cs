ef88addb00607bb44f942df1bd859cafd1aa2bf2
Stephane Gilles <sag48@njit.edu>
2023-03-14 16:41:54 -0400

    solved Reverse Words in a String III (c#); could have realized that c# can split like python sooner rather than later

---
+++ Reverse Words in a String III/Reverse Words in a String III/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Reverse_Words_in_a_String_III
+{
+    public class Solution
+    {
+        public static string ReverseWords(string s)
+        {
+            string res = "";
+            string[] words = s.Split(' ');
+            int counter = 0;
+            foreach (string word in words)
+            {
+                string reverse = "";
+                for (int i = word.Length - 1; i >= 0; i--)
+                {
+                    reverse += word[i];
+                }
+                counter++;
+                if (counter == words.Length) 
+                    res += reverse;
+                else
+                    res += reverse + " ";
+            }
+
+            return res;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Let's take LeetCode contest";
+            string t = Solution.ReverseWords(s);
+            Console.WriteLine(t);
+        }
+    }
+}

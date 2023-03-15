6ebebe3f12dfa6e9f533623a1a8806a58f9d6fa2
Stephane Gilles <sag48@njit.edu>
2023-03-15 13:47:16 -0400

    solved To Lower Case (c#); straightforward

---
+++ To Lower Case/To Lower Case/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace To_Lower_Case
+{
+    public class Solution
+    {
+        public static string ToLowerCase(string s)
+        {
+            string res = "";
+            foreach (char c in s)
+            {
+                if (Char.IsUpper(c))
+                    res += Convert.ToChar(c + 32);
+                else
+                    res += c;
+            }
+            return res;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // chr(65) = 'A'
+            // chr(97) = 'a'
+            // difference: 32
+            string s = "LOVELY";
+            string t = Solution.ToLowerCase(s);
+            Console.WriteLine(t);
+        }
+    }
+}

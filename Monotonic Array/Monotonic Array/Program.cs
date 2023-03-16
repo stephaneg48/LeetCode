4539784d6489948c1f5b816c2d742b3ac3b58af1
Stephane Gilles <sag48@njit.edu>
2023-03-16 14:11:51 -0400

    solved Monotonic Array (c#); felt like a silly solution, but didn't feel so bad when the few test cases it failed in involved an array length of one

---
+++ Monotonic Array/Monotonic Array/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Monotonic_Array
+{
+    public class Solution
+    {
+        public static bool IsMonotonic(int[] nums)
+        {
+            if (nums.Length == 1)
+                return true;
+
+            bool res = false;
+            for (int i = 0; i < nums.Length; i++) // increasing
+            {
+                if (i == 0)
+                    continue;
+                else
+                {
+                    if (nums[i] >= nums[i - 1])
+                        res = true;
+                    else
+                    {
+                        res = false; 
+                        break;
+                    }
+                }
+            }
+
+            if (res)
+                return res;
+            else
+            {
+                for (int i = 0; i < nums.Length; i++) // decreasing
+                {
+                    if (i == 0)
+                        continue;
+                    else
+                    {
+                        if (nums[i] <= nums[i - 1])
+                            res = true;
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 6, 5, 4, 4 };
+            bool res = Solution.IsMonotonic(nums);
+            Console.WriteLine(res);
+        }
+    }
+}

86ca435cc3ffd823c25d270b09f55884c19ac6e2
Stephane Gilles <sag48@njit.edu>
2023-03-15 22:43:23 -0400

    solved Most Common Word, Self Dividing Numbers (c#); self dividing numbers was straightforward, most common word was a good memory usage consideration

---
+++ Most Common Word/Most Common Word/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Most_Common_Word
+{
+    public class Solution
+    {
+        public static string MostCommonWord(string paragraph, string[] banned)
+        {
+            string res = "";
+
+            // trim the words first
+            char[] punctuation = { '!', '?', '\'', ',', ';', '.', ' ' };
+            string[] words = paragraph.Split(punctuation);
+            
+            for (int i = 0; i < words.Length; i++)
+                words[i] = words[i].ToLower(); // interestingly, can't do this with a foreach loop
+
+            // remove null entries that came as result of loop
+            words = words.Where(word => !String.IsNullOrEmpty(word)).ToArray();
+
+            // use hashmap; ignore banned words
+            Dictionary<string, int> keyValuePairs = new Dictionary<string, int>();
+
+            foreach (string word in words) 
+            {
+                if (keyValuePairs.ContainsKey(word) && !banned.Contains(word))
+                    keyValuePairs[word]++;
+                else if (!keyValuePairs.ContainsKey(word) && !banned.Contains(word))
+                    keyValuePairs[word] = 1;
+            }
+
+            // get the max
+            foreach(string key in keyValuePairs.Keys)
+            {
+                if (keyValuePairs[key] == keyValuePairs.Values.Max())
+                {
+                    res = key;
+                    break;
+                }
+            }
+
+            return res;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string paragraph = "a, a, a, a, b,b,b,c, c";
+            string[] banned = { "a" };
+            string result = Solution.MostCommonWord(paragraph, banned);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Self Dividing Numbers/Self Dividing Numbers/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Self_Dividing_Numbers
+{
+    public class Solution
+    {
+        public static IList<int> SelfDividingNumbers(int left, int right)
+        {
+            int[] res = new int[left + right];
+            int j = 0;
+            for (var i = left; i <= right; i++)
+            {
+                char[] chars = i.ToString().ToCharArray();
+                int size = chars.Length;
+                int counter = 0;
+                foreach (char digit in chars)
+                {
+                    var value = (int)Char.GetNumericValue(digit);
+                    if (value != 0 && i % value == 0)
+                        counter++;
+                }
+                if (counter == size)
+                {
+                    res[j] = i;
+                    j++;
+                }
+            }
+
+            res = res.Where(val => val != 0).ToArray();
+            return res;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int left = 1, right = 22;
+            var result = Solution.SelfDividingNumbers(left, right);
+            foreach (var num in result)
+            {
+                Console.WriteLine(num);
+            }    
+        }
+    }
+}

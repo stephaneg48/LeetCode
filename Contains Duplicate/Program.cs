b06e628bfdfdf4d214b00029c976e3dd8e8c5da9
Stephane Gilles <sag48@njit.edu>
2023-06-04 20:59:35 -0400

    solved Contains Duplicate (C#); straightforward hashmap

---
+++ Contains Duplicate/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Contains_Duplicate
+{
+    public class Solution
+    {
+        public static bool ContainsDuplicate(int[] nums) 
+        {
+            // use hashmap
+            Dictionary<int, int> counters = new Dictionary<int, int>();
+
+            foreach (int num in nums)
+            {
+                if (counters.ContainsKey(num))
+                {
+                    counters[num]++;
+                    if (counters[num] > 1)
+                        return true;
+                }
+                else
+                    counters[num] = 1;
+            }
+            return false;
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {1,1,1,3,3,4,3,2,4,2};
+            bool result = Solution.ContainsDuplicate(nums);
+            Console.WriteLine(result);
+        }
+    }
+}

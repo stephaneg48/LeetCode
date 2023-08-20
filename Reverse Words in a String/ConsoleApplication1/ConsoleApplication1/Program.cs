2f26f72cfd7892b12f8c6a5a42fef91446232b6c
Stephane Gilles <sag48@njit.edu>
2023-08-20 10:26:11 -0400

    solved Reverse Words in a String (c#); surprised that the third version of this is labeled easy, but this one is medium

---
+++ Reverse Words in a String/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Solution
+    {
+        public static string ReverseWords(string s)
+        {
+            string res = "";
+            string[] words = s.Split(' ');
+            int counter = 0;
+
+            words = words.Where(word => !String.IsNullOrEmpty(word)).ToArray();
+
+            for (int i = words.Length - 1; i >= 0; i--)
+            {
+                counter++;
+                if (counter == words.Length)
+                    res += words[i];
+                else
+                    res += words[i] + " ";
+            }
+
+            return res;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "  hello world  ";
+            string result = Solution.ReverseWords(s);
+            Console.WriteLine(result);
+        }
+    }
+}

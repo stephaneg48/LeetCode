f46d71527cb6eb3bc75abc5f7bdc31afe7ce1734
Stephane Gilles <sag48@njit.edu>
2023-08-22 22:51:20 -0400

    solved Majority Element (c#); straightforward hashmap

---
+++ Majority Element/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Solution
+    {
+        public static int MajorityElement(int[] nums)
+        {
+            Dictionary<int, int> counters = new Dictionary<int, int>();
+            int res = 0;
+
+            foreach (int num in nums)
+                if (counters.ContainsKey(num))
+                    counters[num]++;
+                else
+                    counters[num] = 1;
+
+            foreach (int key in counters.Keys)
+                if (counters[key] > (int)Math.Floor((double)nums.Length / 2))
+                    res = key;
+
+            return res;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 2, 1, 1, 1, 2, 2 };
+            int result = Solution.MajorityElement(nums);
+            Console.WriteLine(result);
+        }
+    }
+}
